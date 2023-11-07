02a979bd23cbba53401cfdafb5aa8248b2576237
LGF-LGF <li_gui_feng@163.com>
2023-11-07 10:27:09 +0800

    修改

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 ```shell
 bash <(curl -Ls https://raw.githubusercontent.com/LGF-LGF/tools/main/tools.sh)
 ```
-|变量/函数|作用|
-|controls|使用什么安装器|
-|release|系统版本|
+| 变量/函数 | 作用           |
+| --------- | -------------- |
+| controls  | 使用什么安装器 |
+| release   | 系统版本       |
+
+
--- tools.sh
+++ tools.sh
@@ -6,6 +6,7 @@ yellow='\033[33m'
 plain='\033[0m'
 
 #统一配置变量，不清楚原理保持默认
+#安装包下载路径，例如下载nginx，nginx安装包路径：$download_path/nginx/
 download_path=/tools/soft/
 #注：这里为所有安装软件的统一路径，任何软件都会以软件名在这个路径下创建路径安装，路径重复根据date +%Y%m%d进行备份
 install_path=/usr/local/soft/
@@ -19,9 +20,6 @@ nginx_download_url=https://nginx.org/download/nginx-1.24.0.tar.gz
 #程序用户，无法登陆
 user=nginx
 #END
-
-
-
 
 #check systemctl version
 if [[ -f /etc/redhat-release ]]; then
@@ -39,7 +37,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    LOGE "未检测到系统版本，请联系脚本作者！\n" && exit 1
+    echo -e  "${red}未检测到系统版本，请联系脚本作者！\n${plain}" && exit 1
 fi
 # select erector
 controls=''
@@ -51,9 +49,54 @@ elif [ "$release" == "debian" ]; then
   controls='apt'
 fi
 
+
+function check_install_system() {
+    #test_server_port=() 检查此数组中的端口
+    netstat -ntpl|grep LISTEN|awk '{print $4}' >/opt/test_sys.txt
+    #process=(nginx) 检查此数组中的进程
+        num=0
+        for line in `cat /opt/test_sys.txt`
+        do
+            sys_port=${line##*:}
+            for i in ${test_server_port[*]}
+            do
+                if [ $sys_port -eq $i ];then
+                    echo -e "\033[31m $sys_port端口已存在，占用服务端口 \033[0m"
+                    let  num=$num+1
+                fi
+            done
+        done
+        u=0
+        if [ $num -eq 0 ]
+        then
+            for pro in ${process[@]}
+            do
+                if [ `ps -ef|grep $pro |grep -v "grep"|wc -l` -ne 0 ]
+                then
+                    echo "$pro有残余进程，删除后再次执行脚本检测安装环境"
+    				exit 1
+                    let  u=$u+1
+                fi
+            done
+            for pro in ${PRODUCT_ORDER[@]}
+            do
+                if [ `ps -ef|grep $pro |grep -v "grep"|wc -l` -ne 0 ]
+                then
+                    echo "服务有残余进程，删除后再次执行脚本检测安装环境"
+                    let  u=$u+1
+                fi
+            done
+        else
+          select=''
+          read -p "是否继续安装，继续安装可能会无法启动（y/n）:" select
+            if [ ${select} != y ]; then
+            exit 1
+            fi
+        fi
+} #check_install_nginx_system
 function install_nginx() {
     select=''
-    $controls install -y wget curl 
+    $controls install -y wget curl net-tools
     if [ $? -ne 0 ];then
       echo -e "${red}安装失败${plain}" && exit 0
     fi
@@ -130,7 +173,7 @@ function install_nginx() {
         echo -e "${green}安装完成...${plain}"
         cd $NGINX_HOME && cd .. && rm -rf nginx_file
         else
-        echo -e "${red}安装失败...${plain}'"
+        echo -e "${red}安装失败...${plain}"
         exit 0
     fi
 
@@ -176,7 +219,9 @@ else
 fi
 
 
-}
+} #install_nginx
+
+
 select=''
 function show_Use() {
     select=''
@@ -192,12 +237,23 @@ function show_Use() {
       ;;
     *)
       echo "输入错误"
+      exit 1
       ;;
     esac
 }
 
 function show_soft() {
+#安装软件进程和端口检查，调用函数check_install_system,调用前设置
+#    #process=() 检查此数组中的进程
+#    #test_server_port=() 检查此数组中的端口
+    process=()
+    test_server_port=()
     select=''
+    if [ $? -eq 0 ];then
+      echo -e "${green}变量初始化完成${plain}"
+      else
+      echo -e "${red}变量初始化失败${plain}"
+    fi
     echo -e "${green}0. ${plain}返回主页面."
     echo -e "${green}1. ${plain}Nginx."
     read -p   "输入序号【0-1】：" select
@@ -206,10 +262,18 @@ function show_soft() {
      return
       ;;
     1)
+     process=(nginx)
+     test_server_port=(80 443)
+     check_install_system
      install_nginx
       ;;
+    *)
+          echo "输入错误"
+          exit 1
+          ;;
     esac
-}
+} #show_soft
+
 
 [ `whoami` != root ] && echo -e "${red}需要使用root权限${plain}" && exit 1
 while [ true ]; do
