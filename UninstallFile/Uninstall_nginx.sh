bf280556a89c1bbaf16088ac0e784b371a8299c3
LGF-LGF <li_gui_feng@163.com>
2023-11-26 23:35:29 +0800

    增加Nginx卸载功能

--- UninstallFile/Uninstall_nginx.sh
+++ UninstallFile/Uninstall_nginx.sh
@@ -21,46 +21,12 @@ if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ]; th
         let killall_select++
     done
     [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ] && echo "Nginx进程杀死失败，退出..." && exit 0
-    printf "获取Nginx安装路径："
+printf "获取Nginx安装路径："
     echo $NGINX_HOME
     if [ -z $NGINX_HOME ]; then
         command -v nginx
-       if [ `command -v nginx` ==  /usr/sbin/nginx ];then
-        $controls remove -y nginx
-        $controls autoremove -y nginx
-        source /etc/profile
-       fi
-    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
-    systemctl disable nginx.service &>/dev/null
-    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
-    systemctl daemon-reload
-    echo "卸载nginx完成"
-    elif [ ! -z $NGINX_HOME ];then
-    rm -rf $NGINX_HOME
-    sed -i "/NGINX/d" /etc/profile
-    sed -i "/nginx/d" /etc/profile
-    source /etc/profile
-    if [ `command -v nginx` ==  /usr/sbin/nginx ];then
-            $controls remove -y nginx
-            $controls autoremove -y nginx
-            source /etc/profile
-    fi
-    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
-    systemctl disable nginx.service &>/dev/null
-    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
-    systemctl daemon-reload
-    echo "卸载nginx完成"
-
-    else
-    echo "未获取到Nginx路径"
-
-    fi
-else
-    printf "获取Nginx安装路径："
-    echo $NGINX_HOME
-    if [ -z $NGINX_HOME ]; then
-        command -v nginx
-       if [ `command -v nginx` ==  /usr/sbin/nginx ];then
+    if [ "$(command -v nginx)" == " "  ]; then
+       if [ "$(command -v nginx)" == "/usr/sbin/nginx" ];then
         $controls remove -y nginx
         $controls autoremove -y nginx
         source /etc/profile
@@ -88,4 +54,42 @@ else
     else
         echo "未获取到Nginx路径"
     fi
+    fi
+elif [ ! -z $NGINX_HOME ] || [ ! -z $(command -v nginx) ]; then
+    printf "获取Nginx安装路径："
+    echo $NGINX_HOME
+    if [ -z $NGINX_HOME ]; then
+        command -v nginx
+    if [ "$(command -v nginx)" == " "  ]; then
+       if [ "$(command -v nginx)" == "/usr/sbin/nginx" ];then
+        $controls remove -y nginx
+        $controls autoremove -y nginx
+        source /etc/profile
+       fi
+    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
+    systemctl disable nginx.service &>/dev/null
+    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
+    systemctl daemon-reload
+    echo "卸载nginx完成"
+    elif [ ! -z $NGINX_HOME ];then
+    rm -rf $NGINX_HOME
+    sed -i "/NGINX/d" /etc/profile
+    sed -i "/nginx/d" /etc/profile
+    source /etc/profile
+    if [ "$(command -v nginx)" == "/usr/sbin/nginx" ]; then
+        $controls remove -y nginx
+        $controls autoremove -y nginx
+        source /etc/profile
+    fi
+    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
+    systemctl disable nginx.service &>/dev/null
+    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
+    systemctl daemon-reload
+    echo "卸载nginx完成"
+    else
+        echo "未获取到Nginx路径"
+    fi
+    fi
+else
+  echo "未检测到Nginx安装信息"
 fi
