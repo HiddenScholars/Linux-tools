56f2f4397e95fb9853c7705712b1894cee5b7cc4
LGF-LGF <li_gui_feng@163.com>
2023-11-26 23:12:13 +0800

    增加Nginx卸载功能

--- UninstallFile/Uninstall_nginx.sh
+++ UninstallFile/Uninstall_nginx.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-uninstall_select=''
-read -p "确认卸载（y/n）" $uninstall_select
-[ ! "${uninstall_select}" == "y" ] && return
+
+read -p "回车后确认卸载："
+
 
 source /etc/profile
 source /tools/config.sh
 if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ]; then
-    echo "检测到Nginx进程，进程ID：`ps -ef | grep nginx | grep -v grep | awk '{print $2}'`"
+    printf "检测到Nginx进程，进程ID：`ps -ef | grep nginx | grep -v grep | awk '{print $2}'`"
     killall_select=1
     while [ $killall_select -lt 4 ]; do
-      echo "开始killNginx进程 $killall_select次尝试"
+      echo "开始kill Nginx进程 $killall_select"
+        if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ]; then
         for i in `ps -ef | grep nginx | grep -v grep | awk '{print $2}'`
             do
               kill -9 $i
             done
+        fi
         sleep 2
-       if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ];then
-          let killall_select++
-       elf [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` == 0 ];then
-          killall_select=3
-       fi
+        let killall_select++
     done
     [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ] && echo "Nginx进程杀死失败，退出..." && exit 0
     printf "获取Nginx安装路径："
@@ -30,15 +28,23 @@ if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ]; th
        if [ `command -v nginx` ==  /usr/sbin/nginx ];then
         $controls remove -y nginx
         $controls autoremove -y nginx
+        source /etc/profile
        fi
     [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
     systemctl disable nginx.service &>/dev/null
     [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
     systemctl daemon-reload
     echo "卸载nginx完成"
-    elf [ ! -z $NGINX_HOME ];then
+    elif [ ! -z $NGINX_HOME ];then
     rm -rf $NGINX_HOME
-    sed -i '$NGINX_HOME/d' /etc/profile
+    temp_var="$NGINX_HOME"
+    sed -i "/$temp_var/d" /etc/profile
+    source /etc/profile
+    if [ `command -v nginx` ==  /usr/sbin/nginx ];then
+            $controls remove -y nginx
+            $controls autoremove -y nginx
+            source /etc/profile
+    fi
     [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
     systemctl disable nginx.service &>/dev/null
     [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
@@ -47,25 +53,33 @@ if [ `ps -ef | grep nginx | grep -v grep | awk '{print $2}' | wc -l ` != 0 ]; th
     fi
 else
     printf "获取Nginx安装路径："
-        echo $NGINX_HOME
-        if [ -z $NGINX_HOME ]; then
-            command -v nginx
-           if [ `command -v nginx` ==  /usr/sbin/nginx ];then
+    echo $NGINX_HOME
+    if [ -z $NGINX_HOME ]; then
+        command -v nginx
+       if [ `command -v nginx` ==  /usr/sbin/nginx ];then
+        $controls remove -y nginx
+        $controls autoremove -y nginx
+        source /etc/profile
+       fi
+    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
+    systemctl disable nginx.service &>/dev/null
+    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
+    systemctl daemon-reload
+    echo "卸载nginx完成"
+    elif [ ! -z $NGINX_HOME ];then
+    rm -rf $NGINX_HOME
+    temp_var="$NGINX_HOME"
+    sed -i "/$temp_var/d" /etc/profile
+    source /etc/profile
+    if [ `command -v nginx` ==  /usr/sbin/nginx ];then
             $controls remove -y nginx
             $controls autoremove -y nginx
-           fi
-        [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
-        systemctl disable nginx.service &>/dev/null
-        [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
-        systemctl daemon-reload
-        echo "卸载nginx完成"
-        elf [ ! -z $NGINX_HOME ];then
-        rm -rf $NGINX_HOME
-        sed -i '$NGINX_HOME/d' /etc/profile
-        [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
-        systemctl disable nginx.service &>/dev/null
-        [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
-        systemctl daemon-reload
-        echo "卸载nginx完成"
-        fi
+            source /etc/profile
+    fi
+    [ -f /etc/init.d/nginx ] && sudo rm -f /etc/init.d/nginx
+    systemctl disable nginx.service &>/dev/null
+    [ -f /usr/lib/systemd/system/nginx.service ] && sudo rm -rf /usr/lib/systemd/system/nginx.service
+    systemctl daemon-reload
+    echo "卸载nginx完成"
+    fi
 fi
