102e9fa4ead7bac3cab9f340d619da0a4f922bb2
LGF-LGF <li_gui_feng@163.com>
2023-12-26 15:19:19 +0800

    增加 1、nginx增加安装版本

--- tools.sh
+++ tools.sh
@@ -15,7 +15,7 @@ config_file=/tools/config.sh
     wget -P ${config_path} https://raw.githubusercontent.com/LGF-LGF/tools/main/config.sh
     [ ! -f ${config_file} ] && echo -e "${red}下载失败，config文件不存在，检查后再次执行脚本!!!${plain}" && exit 0
   else
-    [ `curl -s https://raw.githubusercontent.com/LGF-LGF/tools/main/config.sh | wc -l` -ne `cat $config_file | wc -l ` ];then
+    [ `curl -s https://raw.githubusercontent.com/LGF-LGF/tools/main/config.sh | wc -l` -ne `cat $config_file | wc -l ` ]
       config_select=''
       read -p "config.sh文件有变化，是否重新下载？（y/n）" config_select
       if [ "$config_select" == "y" ];then
