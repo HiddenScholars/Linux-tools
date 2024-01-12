93dd4ac5c431ccd5dc4b50df809d1be5e42dfffb
LGF-LGF <li_gui_feng@163.com>
2024-01-12 23:37:13 +0800

    1、列表增加清除tool命令功能

--- Link_localhost/uninstall.sh
+++ Link_localhost/uninstall.sh
@@ -2,8 +2,10 @@
 source /etc/profile
 if [  -f /etc/init.d/tool ]; then
     rm -rf /etc/init.d/tool && [ -L /usr/bin/tool ] && rm -rf /usr/bin/tool
+    echo -e "${green}删除tool指令${plan}"
 elif [ -L /usr/bin/tool ]; then
     rm -rf /usr/bin/tool
+    echo -e "${green}删除tool软连接${plan}"
 else
   echo -e "${red}tool命令不存在...${plan}"
 fi
