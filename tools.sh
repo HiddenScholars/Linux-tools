63d8bcf7f420d61d6eee000c31c0de72d405ecc9
LGF-LGF <li_gui_feng@163.com>
2024-01-28 18:50:49 +0800

    修正，选择执行地址的问题

--- tools.sh
+++ tools.sh
@@ -29,30 +29,29 @@ done
 
 #config.sh check
 #======================================================================
+  if [ -z $url_address ]; then
+    url_address_number=("raw.githubusercontent.com" "raw.yzuu.cf")
+
+          for i in "${!url_address_number[@]}"
+          do
+              echo "$i：${url_address_number[$i]}"
+          done
+          read -p  "下载参数为空,请选择或手动输入下载地址：" url_address_select
+          if [[ $url_address_select =~ ^[0-9]+$ ]]; then
+              if [ ! -z ${url_address_number[$url_address_select]} ]; then
+                echo ${url_address_number[$url_address_select]}
+                  url_address=${url_address_number[$url_address_select]}
+              else
+                  echo -e "${red}选择的地址不存在${plain}"
+              fi
+          else
+              url_address=$url_address_select
+          fi
+  fi
   if [ ! -f ${config_file} ];then
     [ ! -d ${config_path} ] && mkdir ${config_path}
     echo -e "${red}config文件不存在，开始下载...${plain}"
-    if [ -z $url_address ]; then
-        url_address_number=("raw.githubusercontent.com" "raw.yzuu.cf")
-
-        for i in "${!url_address_number[@]}"
-        do
-            echo "$i：${url_address_number[$i]}"
-        done
-        read -p  "下载参数为空,请选择或手动输入下载地址：" url_address_select
-        if [[ $url_address_select =~ ^[0-9]+$ ]]; then
-            if [ ! -z ${url_address_number[$url_address_select]} ]; then
-              echo ${url_address_number[$url_address_select]}
-                url_address=${url_address_number[$url_address_select]}
-            else
-                echo -e "${red}选择的地址不存在${plain}"
-            fi
-        else
-            url_address=$url_address_select
-        fi
-
         wget -P ${config_path} https://$url_address/HiddenScholars/Linux-tools/$con_branch/config.sh
-    fi
     [ ! -f ${config_file} ] && echo -e "${red}下载失败，config文件不存在，检查后再次执行脚本!!!${plain}" && exit 0
 
   elif  [ `curl -s https://$url_address/HiddenScholars/Linux-tools/$con_branch/config.sh | wc -l` -gt `cat $config_file | wc -l ` ];then
