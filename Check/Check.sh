5ac12a41b5badd40cd8a416df45a368332286d7a
master <li_gui_feng@163.com>
2024-03-08 00:55:29 +0800

    更新-增加通过curl请求脚本对指定软件进行识别

--- Check/Check.sh
+++ Check/Check.sh
@@ -33,52 +33,66 @@ PACKAGE_MASTER
 SYSTEM_CHECK
 #DIRECTIVES_CHECK
 #DIRECTIVES=() #install list
-#NOTFONUDDIRECTIVES=() #install faliled
+#NOTFONUDDIRECTIVES=() #install failed
 function DIRECTIVES_CHECK() {
-    local NOTFONUDDIRECTIVES_EXEC=$1 #$1==0 repo install
-    local GET_WHICH=$(command -v which | wc -l)
-    if [ "${GET_WHICH}" != 0 ]; then
+    if [ "$1" == 0 ]; then
+       local NOTFONUDDIRECTIVES_EXEC=$1 #$1==0 repo install
+       if [ -n "$2" ];then
+           shift
+           DIRECTIVES=("$@")
+       fi
+    else
+       DIRECTIVES=("$@")
+
+    fi
+    GET_WHICH=$(command -v which | wc -l)
+    if [ "${GET_WHICH}" != 0 ] && [ "${#DIRECTIVES[@]}" != 0 ]; then
         for i in "${DIRECTIVES[@]}"
         do
           which "$i" &>/dev/null
           if [ $? -ne 0  ]; then
               NOTFONUDDIRECTIVES+=("$i")
+             [ "${#NOTFONUDDIRECTIVES[@]}" != 0 ] && printf  "%s,\t Not installed." "$i"
           fi
         done
+    else
+      echo "which not found."
+      exit 1
     fi
     if [ -n "$NOTFONUDDIRECTIVES_EXEC" ] && [ "$NOTFONUDDIRECTIVES_EXEC" -eq 0 ]; then
         for (( i = 0; i < "${#NOTFONUDDIRECTIVES[@]}"; i++ )); do
-               $controls -y install "$y"
+               $controls -y install "${NOTFONUDDIRECTIVES[$i]}"
                if [ $? -eq 0  ]; then
-                   unset "${NOTFONUDDIRECTIVES[$i]}"
+                   unset "NOTFONUDDIRECTIVES[$i]"
                fi
         done
         if [ "${#NOTFONUDDIRECTIVES[@]}" -ne 0 ]; then
               echo
-              printf "Software package："
+              printf "Software Package："
               for (( i = 0; i < "${#NOTFONUDDIRECTIVES[@]}"; i++ )); do
-                  printf "${NOTFONUDDIRECTIVES[$i]};\t"
+                  printf "%s," "${NOTFONUDDIRECTIVES[$i]}"
               done
-              echo "install failed!"
+              printf "\t install failed!"
+              return 1;
         fi
     fi
 
 }
 function SET_CONFIG() {
    if [ "$controls" != "-1" ] && [ "$SystemVersion" != "-1" ] && [ "$CPUArchitecture" == "x86_64" ] && [ -f "$config_file" ]; then
-        GET_CONTROLS=$(cat "$config_file" | grep controls= | wc -l)
+        GET_CONTROLS=$(grep -c 'controls=' $config_file)
         if [ "$GET_CONTROLS" == "1" ]; then
            sed -i "s/controls=.*/controls=$controls/g" $config_file
         else
            echo "controls=$controls" >> "$config_file"
         fi
-        GET_CPUArchitecture=$(cat "$config_file" | grep CPUArchitecture= | wc -l)
+        GET_CPUArchitecture=$(grep -c 'CPUArchitecture=' $config_file)
         if [ "$GET_CPUArchitecture" == "1" ]; then
             sed -i "s/CPUArchitecture=.*/CPUArchitecture=$GET_CPUArchitecture/g" $config_file
         else
             echo "CPUArchitecture=$GET_CPUArchitecture" >> $config_file
         fi
-        GET_SystemVersion=$(cat "$config_file" | grep SystemVersion= | wc -l)
+        GET_SystemVersion=$(grep -c 'SystemVersion=' $config_file)
         if [ "$GET_SystemVersion" == "1" ]; then
             sed -i "s/SystemVersion=.*/SystemVersion=$SystemVersion/g" $config_file
         else
@@ -108,5 +122,5 @@ SET_CONFIG)
                   SET_CONFIG
                   ;;
 *)
-                  printf "not found";
+                  return 1;
 esac
