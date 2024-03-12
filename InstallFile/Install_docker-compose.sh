2e1a9472f681976cdb274e930d6dd21297d7ec1c
LGF-LGF <li_gui_feng@163.com>
2024-03-12 22:54:06 +0800

    更新

--- InstallFile/Install_docker-compose.sh
+++ InstallFile/Install_docker-compose.sh
@@ -8,11 +8,9 @@ if [ -f /usr/local/bin/docker-compose ];then
    mv /usr/local/bin/docker-compose /usr/local/bin/"$time"_bak
 fi
 bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  docker-compose  $(for i in "${docker_compose_download_urls[@]}";do printf "%s " "$i";done)
-if [ -f "$download_path"/docker-compose/docker-compose ]; then
   if $(cp -rf "$download_path"/docker-compose/docker-compose /usr/local/bin/);then
     chown +x /usr/local/bin/docker-compose
   fi
-fi
 
 if $(docker-compose -v) &>/dev/null; then
    echo -e "${green}docker_compose安装完成${plain}"
--- UpdateFile/UPDATE.sh
+++ UpdateFile/UPDATE.sh
@@ -2,8 +2,9 @@
 source /tools/config &>/dev/null
     if [ -f /tools/config ]; then
         mv /tools/config /tools/config_bak
-        wget -P /tools/ https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/config
-        if [ $? -eq 0 ]; then
+        if wget -P /tools/ https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/config; then
+            sed -i "s/download_path=.*/download_path=$download_path=/g" /tools/config
+            sed -i "s/install_path=.*/install_path=$install_path=/g" /tools/config
             sed -i "s/url_address=.*/url_address=$url_address/g" /tools/config #下载完成后修改仓库地址
             sed -i "s/con_branch=.*/con_branch=$con_branch/g" /tools/config #下载完成后修改分支
             rm -rf /tools/config_bak
