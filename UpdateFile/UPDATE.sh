50069d688069537df22f0d1621df2fd6b83c8f36
LGF-LGF <li_gui_feng@163.com>
2024-03-13 12:59:13 +0800

    更新

--- UpdateFile/UPDATE.sh
+++ UpdateFile/UPDATE.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 source /tools/config &>/dev/null
-    if [ -f /tools/config ]; then
-            sed -i "s/download_path=.*/download_path=$download_path=/g" /tools/config
-            sed -i "s/install_path=.*/install_path=$install_path=/g" /tools/config
+     if [ -f /tools/config ]; then
+        mv /tools/config /tools/config_bak
+        wget -P "$config_path" https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/config
+        if [ $? -eq 0 ]; then
             sed -i "s/url_address=.*/url_address=$url_address/g" /tools/config #下载完成后修改仓库地址
             sed -i "s/con_branch=.*/con_branch=$con_branch/g" /tools/config #下载完成后修改分支
+            rm -rf /tools/config_bak
+        fi
     else
        echo "not found config."
-       return 1
+       exit 0
     fi
     bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Link_localhost/uninstall.sh) # tool link uninstall.sh
     bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Link_localhost/install.sh) # tool link install.sh
