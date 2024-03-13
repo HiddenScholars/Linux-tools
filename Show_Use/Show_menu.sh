a63322c63567b74ad2852eb3e90e6707f5ad1709
LGF-LGF <li_gui_feng@163.com>
2024-03-13 22:17:30 +0800

    更新

--- Show_Use/Show_menu.sh
+++ Show_Use/Show_menu.sh
@@ -21,8 +21,8 @@ trap handle_exit EXIT
 #菜单目录显示控制
 show_use=("退出" "安装" "卸载" "升级" "acme脚本(搭配cloudflare)" "检查更新")
 show_use_function=("exit 0" "show_Soft" "soft_Uninstall" "soft_Upgrade" "setting_ssl" "check_update")
-show_soft=("返回主页面" "nginx" "docker+docker-compose" "docker-compose" "jdk" "一键安装所有")
-show_soft_function=("return" "install_nginx" "install_docker" "install_docker_compose" "install_jdk" "install_all")
+show_soft=("返回主页面" "nginx" "docker+docker-compose" "docker-compose" "jdk" "mysql5" "一键安装所有")
+show_soft_function=("return" "install_nginx" "install_docker" "install_docker_compose" "install_jdk" "install_mysql5" "install_all")
 soft_uninstall=("返回主页面" "nginx卸载" "docker+docker-compose卸载" "tool命令卸载")
 soft_uninstall_function=("return" "uninstall_nginx" "uninstall_docker_docker_compose" "uninstall_tool")
 soft_upgrade=("返回主菜单" "Nginx平滑升(降)级")
@@ -79,6 +79,10 @@ function uninstall_tool() {
 }
 function install_jdk() {
     bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/InstallFile/Install_jdk.sh)
+}
+
+function install_mysql5() {
+    bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/InstallFile/Install_mysql5.sh)
 }
 function install_all() {
 for i in "${show_soft_function[@]}"
