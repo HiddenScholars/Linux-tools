57914392bd1cdbdbce3e26e3a64fb09de52e9309
LGF-LGF <li_gui_feng@163.com>
2024-03-14 13:37:58 +0800

    更新

--- InstallFile/Install_docker-compose.sh
+++ InstallFile/Install_docker-compose.sh
@@ -8,12 +8,17 @@ if [ -f /usr/local/bin/docker-compose ];then
    mv /usr/local/bin/docker-compose /usr/local/bin/"$time"_bak
 fi
 bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  docker-compose  $(for i in "${docker_compose_download_urls[@]}";do printf "%s " "$i";done)
+
   if $(cp -rf "$download_path"/docker-compose/docker-compose /usr/local/bin/);then
-    chown +x /usr/local/bin/docker-compose
+    echo "复制完成"
+    sudo chmod +x /usr/local/bin/docker-compose
+    if $(docker-compose --version) &>/dev/null; then
+        echo -e "${green}docker_compose安装完成${plain}"
+    else
+        echo -e "${red}docker_compose安装失败${plain}'"
+    fi
+  else
+    printf "复制失败\n安装失败\n"
   fi
 
-if $(docker-compose --version) &>/dev/null; then
-   echo -e "${green}docker_compose安装完成${plain}"
-else
-   echo -e "${red}docker_compose安装失败${plain}'"
-fi
+
