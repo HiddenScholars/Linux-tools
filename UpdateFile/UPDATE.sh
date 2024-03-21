e2245a7f37a8b5a1b9d5142a53d84f0dba815acf
LGF-LGF <li_gui_feng@163.com>
2024-03-21 10:11:13 +0800

    增加执行进度条

--- UpdateFile/UPDATE.sh
+++ UpdateFile/UPDATE.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+function progress_bar() {
+    local total_functions=$1  # 总函数数量
+    local executed_functions=$2  # 已执行的函数数量
+    local progress=$((executed_functions * 100 / total_functions))  # 计算进度百分比
+    printf "\rProgress: [%-50s] %d%%" $(printf '#%.0s' $(seq 1 $((progress / 2)))) $progress
+}
 source /tools/config &>/dev/null
      if [ -f /tools/config ]; then
         mv /tools/config /tools/config_bak
@@ -8,8 +15,11 @@ source /tools/config &>/dev/null
             sed -i "s/con_branch=.*/con_branch=$con_branch/g" /tools/config #下载完成后修改分支
             rm -rf /tools/config_bak
             bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Link_localhost/uninstall.sh) # tool link uninstall.sh
+            progress_bar 3 1
             bash <(curl -sL https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Link_localhost/install.sh) # tool link install.sh
+            progress_bar 3 2
             bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) SET_CONFIG
+            progress_bar 3 3
         else
             echo "download failed"
             mv /tools/config_bak /tools/config
