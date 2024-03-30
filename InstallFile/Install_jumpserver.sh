bdc6e074f2fbb43e405bc5380c079607448a169b
LGF-LGF <li_gui_feng@163.com>
2024-03-31 03:13:10 +0800

    更新

--- InstallFile/Install_jumpserver.sh
+++ InstallFile/Install_jumpserver.sh
@@ -14,29 +14,28 @@ if [ "$edit_config_txt" == "y" ]; then
     config_path=/opt/jumpserver/config/config.txt
     reload_select=''
     read -rp "定义可信任的访问 IP, 请根据实际情况修改, 如果是公网 IP 请改成对应的公网 IP（default: null）：" edit_config_txt_DOMAIN
-    [ -n "$edit_config_txt_DOMAIN" ] && sed -i "s/DOMAIN=.*/DOMAIN=$edit_config_txt_DOMAIN/g" $config_path
+    [ -n "$edit_config_txt_DOMAIN" ] && sed -i "s/DOMAINS=.*/DOMAINS=$edit_config_txt_DOMAIN/g" $config_path
     read -rp "开启HTTPS访问(y/n)（default：不设置）：" edit_config_txt_HTTPS
     if [ "$edit_config_txt_HTTPS" == "y" ];then
-      sed -i "s/#HTTPS_PORT=.*/HTTPS_PORT=443/g" $config_path
-      sed -i "s/#SERVER_NAME=.*/SERVER_NAME=$edit_config_txt_DOMAIN/g" $config_path
+      sed -i "s/# HTTPS_PORT=.*/HTTPS_PORT=443/g" $config_path
+      sed -i "s/# SERVER_NAME=.*/SERVER_NAME=$edit_config_txt_DOMAIN/g" $config_path
       if [ -f /opt/jumpserver/config/nginx/cert/server.crt ]; then
-          sed -i "s/#SSL_CERTIFICATE=.*/SSL_CERTIFICATE=/opt/jumpserver/config/nginx/cert/server.crt/g" $config_path
+          sed -i 's|# SSL_CERTIFICATE=your_cert|SSL_CERTIFICATE=/opt/jumpserver/config/nginx/cert/server.crt|g' "$config_path"
       else
          echo "server.crt not found"
       fi
       if [ -f /opt/jumpserver/config/nginx/cert/server.key ]; then
-         sed -i "s/#SSL_CERTIFICATE_KEY=.*/SSL_CERTIFICATE_KEY=/opt/jumpserver/config/nginx/cert/server.key/g" $config_path
+         sed -i 's|# SSL_CERTIFICATE_KEY=your_cert_key|SSL_CERTIFICATE_KEY=/opt/jumpserver/config/nginx/cert/server.key|g' $config_path
       else
          echo "server.key not found"
       fi
-      read -rp "脚本预备修改操作完成，是否现在重新配置（y/n）" reload_select
+      read -rp "脚本预备修改操作完成，是否现在重载配置（y/n）" reload_select
       if [ "$reload_select" == "y" ]; then
-          docker_id=($(docker ps -aq --filter "name=jms.*"))
-          if [ "${#docker_id[@]}" -ne 0 ]; then
-              for i in "${docker_id[@]}"
-              do
-                 docker restart "$i"
-              done
+          jmsctl_path=$(find / -name jmsctl.sh)
+          if [ -f "$jmsctl_path" ]; then
+             $jmsctl_path start
+          else
+            echo "jmsctl.sh not found"
           fi
       fi
 
--- UninstallFile/Uninstall_jumpserver.sh
+++ UninstallFile/Uninstall_jumpserver.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-
-docker_id=($(docker ps -aq --filter "name=jms.*"))
-images_id=()
-for i in "${docker_id[@]}"
-do
-  images_id+=($(docker inspect -f '{{.Image}}' "$i" | awk -F':' '{print $2}'))
-done
-          if [ "${#docker_id[@]}" -ne 0 ]; then
-              for y in "${docker_id[@]}"
-              do
-                 docker stop "$y"
-                 docker rm "$y"
-              done
-          fi
-          if [ "${#images_id[@]}" -ne 0 ]; then
-              for t in "${images_id[@]}"
-              do
-                docker rmi "$t"
-              done
-          fi
-if [ -d /opt/jumpserver/ ]; then
-    rm -rf /opt/jumpserver/*
+jmsctl_path=$(find / -name jmsctl.sh)
+if [ -f "$jmsctl_path" ]; then
+    $jmsctl_path uninstall
+else
+  echo "jmsctl.sh not found"
 fi
-
-echo "卸载完成"
