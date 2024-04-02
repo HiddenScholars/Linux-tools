9474c7617dfe626dfd6f93b19f3249f3f983f151
LGF-LGF <li_gui_feng@163.com>
2024-04-02 14:04:20 +0800

    更新

--- UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
+++ UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-   set -x
-if [ -f /tools/unpack_file/"$GET_missing_dirs_lnmp"/uninstall.sh ]; then
+
+if [ -n "$GET_missing_dirs_lnmp" ] && [ -f /tools/unpack_file/"$GET_missing_dirs_lnmp"/uninstall.sh ]; then
    cd /tools/unpack_file/"$GET_missing_dirs_lnmp"/ && bash uninstall.sh
 else
    echo "选择需要lnmp版本的卸载脚本"
