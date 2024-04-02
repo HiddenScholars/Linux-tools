8caaadb937a251c9a1c5c5cf47bece278d4ddfe9
LGF-LGF <li_gui_feng@163.com>
2024-04-02 13:47:13 +0800

    更新

--- InstallFile/Install_lnmp_lamp_lnmpa.sh
+++ InstallFile/Install_lnmp_lamp_lnmpa.sh
@@ -2,11 +2,14 @@
 
 source /tools/config &>/dev/null
 function download_lnmp_package() {
-  bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  lnmp2.0  "$lnmp_package_urls"
+  echo "选择lnmp安装包版本"
+  for (( i = 0; i < "${#lnmp_package_urls[@]}"; i++ )); do
+      bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  lnmp  "${lnmp_package_urls[$i]}"
+  done
   #解压目录检测
   GET_missing_dirs_lnmp=$(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh | bash -s -- check_unpack_file_path)
   echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" Start unzipping."
-  tar xvf "$download_path"/lnmp2.0/lnmp2.0 -C /tools/unpack_file/"$GET_missing_dirs_lnmp" --strip-components 1 &>/dev/null
+  tar xvf "$download_path"/lnmp/lnmp -C /tools/unpack_file/"$GET_missing_dirs_lnmp" --strip-components 1 &>/dev/null
   echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" The decompression is complete."
 }
 case $1 in
