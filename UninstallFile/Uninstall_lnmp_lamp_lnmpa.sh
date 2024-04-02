f3ef967ac146db0cb31b110573b88efa53fe4ce3
LGF-LGF <li_gui_feng@163.com>
2024-04-02 13:54:09 +0800

    更新

--- UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
+++ UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -f /tools/unpack_file/"$GET_missing_dirs_nginx"/uninstall.sh ]; then
-  bash /tools/unpack_file/"$GET_missing_dirs_nginx"/uninstall.sh
+   cd /tools/unpack_file/"$GET_missing_dirs_lnmp"/ && bash uninstall.sh
 else
-  echo "选择需要lnmp版本的卸载脚本"
-   bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  lnmp2.0  $(for i in "${lnmp_package_urls[@]}";do printf "%s " "$i";done)
+   echo "选择需要lnmp版本的卸载脚本"
+   bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  lnmp  $(for i in "${lnmp_package_urls[@]}";do printf "%s " "$i";done)
    GET_missing_dirs_lnmp=$(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh | bash -s -- check_unpack_file_path)
    cd /tools/unpack_file/"$GET_missing_dirs_lnmp"/ && bash uninstall.sh
 fi
