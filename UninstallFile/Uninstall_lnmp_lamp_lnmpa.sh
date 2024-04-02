a1357b295ace2f2d598e6ed69427df7420a15de0
LGF-LGF <li_gui_feng@163.com>
2024-04-02 14:38:42 +0800

    更新

--- UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
+++ UninstallFile/Uninstall_lnmp_lamp_lnmpa.sh
@@ -7,5 +7,6 @@ else
    echo "选择需要lnmp版本的卸载脚本"
    bash <(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh) PACKAGE_DOWNLOAD  lnmp  $(for i in "${lnmp_package_urls[@]}";do printf "%s " "$i";done)
    GET_missing_dirs_lnmp=$(curl -sl https://"$url_address"/HiddenScholars/Linux-tools/"$con_branch"/Check/Check.sh | bash -s -- check_unpack_file_path)
+   tar xvf "$download_path"/lnmp/lnmp -C /tools/unpack_file/"$GET_missing_dirs_lnmp" --strip-components 1 &>/dev/null
    cd /tools/unpack_file/"$GET_missing_dirs_lnmp"/ && bash uninstall.sh
 fi
