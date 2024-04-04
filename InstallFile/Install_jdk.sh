f169f7877853dde98c08f8ebacdfd31319f5b309
LGF-LGF <li_gui_feng@163.com>
2024-04-04 21:55:41 +0800

    更新jdk配置环境变量命令

--- InstallFile/Install_jdk.sh
+++ InstallFile/Install_jdk.sh
@@ -15,32 +15,22 @@ GET_missing_dirs_nginx=$(curl -sl https://"$url_address"/HiddenScholars/Linux-to
     mkdir -p "$jdk_install_path"
     mv /tools/unpack_file/"$GET_missing_dirs_nginx"/* "$jdk_install_path"
     if [ $? -eq 0 ];then
-      echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" 文件复制完成"
+       echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" 文件复制完成"
       "$controls" remove java* openjdk*  -y &>/dev/null
-       set -x
-       source /etc/profile
-       if ! echo "$PATH" | grep -q "$jdk_install_path";then
-         export PATH=$(echo "$PATH" | awk -v RS=: -v ORS=: -v path="$jdk_install_path" '$0 != path' | sed 's/:$//')
-       fi
-       source /etc/profile
-       sed -i "\|$jdk_install_path|d" /etc/profile
-       sed -i "\|JAVA_HOME|d" /etc/profile
 
+       source /etc/profile
+       sed -i "\|export JAVA_HOME=$jdk_install_path|d" /etc/profile
+       sed -i "\|export PATH=.*|d" /etc/profile
+       sed -i "\|export CLASSPATH=.*|d" /etc/profile
+
+       echo "export PATH=$jdk_install_path/bin:$PATH" >>/etc/profile
        echo "export JAVA_HOME=$jdk_install_path" >>/etc/profile
-       GET_PATH_PROFILE=$(echo "$PATH" | grep -cw "$jdk_install_path")
-       if [ "$GET_PATH_PROFILE" == 0 ]; then
-         sed -i "s|PATH=.*|PATH=\$PATH:\$jdk_install_path/bin|g" /etc/profile
-         [ $? -eq 0 ] && echo "成功"
-       fi
-       if [ -n "$CLASSPATH" ]; then
-          sed -i "s|CLASSPATH=.*|CLASSPATH=.:$jdk_install_path/lib/dt.jar:$jdk_install_path/lib/tools.jar|g" /etc/profile
-       else
-          echo "export CLASSPATH=.:$jdk_install_path/lib/dt.jar:$jdk_install_path/lib/tools.jar" >>/etc/profile
-       fi
-       set +x
+       echo "export CLASSPATH=.:$jdk_install_path/lib/dt.jar:$jdk_install_path/lib/tools.jar" >>/etc/profile
           source /etc/profile
-          if $(java -version) && $(javac -version) &>/dev/null; then
+          if $(java -version);then
+            if $(javac -version);then
               echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" 安装成功"
+            fi
           else
               echo ""[$(date '+%Y-%m-%d %H:%M:%S')]" 安装失败"
           fi
